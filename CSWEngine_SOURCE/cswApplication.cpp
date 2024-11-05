3b64de23e29eb655bd1368f6d97b34005d616236
rnrn213 <usan213@gmail.com>
2024-11-06 00:58:10 +0900

    정적 라이브러리 생성 및 응용프로젝트에 연결시키기

---
+++ CSWEngine_SOURCE/cswApplication.cpp
@@ -0,0 +1,1 @@
+#include "cswApplication.h"
--- Editor_Window/main.cpp
+++ Editor_Window/main.cpp
@@ -4,6 +4,8 @@
 #include "framework.h"
 #include "Editor_Window.h"
 
+#include "../CSWEngine_SOURCE/cswApplication.h"
+
 #define MAX_LOADSTRING 100
 
 // 전역 변수:
@@ -42,15 +44,34 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
     MSG msg;
 
-    // 기본 메시지 루프입니다:
-    while (GetMessage(&msg, nullptr, 0, 0))
+    while (true)
     {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
         {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
+            if (msg.message == WM_QUIT)
+                break;
+
+            if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+            {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+            else
+            {
+
+            }
         }
     }
+
+    //// 기본 메시지 루프입니다:
+    //while (GetMessage(&msg, nullptr, 0, 0))
+    //{
+    //    if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+    //    {
+    //        TranslateMessage(&msg);
+    //        DispatchMessage(&msg);
+    //    }
+    //}
 
     return (int) msg.wParam;
 }
