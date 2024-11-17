d9bdf76df70b9294ceee0222fb3cab5a0c556624
rnrn213 <usan213@gmail.com>
2024-11-18 08:30:28 +0900

    LV19 벡터의 덧셈, 뺄셈, 곱셈, 내적, 외적

--- CSWEngine_SOURCE/cswMath.h
+++ CSWEngine_SOURCE/cswMath.h
@@ -2,6 +2,8 @@
 
 namespace csw::math
 {
+#define PI 3.141592f
+
 	struct Vector2
 	{
 		static Vector2 One;
@@ -35,6 +37,46 @@ namespace csw::math
 		Vector2 operator/(float value)
 		{
 			return Vector2(x / value, y / value);
+		}
+
+		void clear()
+		{
+			x = 0.0f;
+			y = 0.0f;
+		}
+
+		float length()
+		{
+			return sqrtf(x * x + y * y);
+		}
+
+		Vector2 normalize()
+		{
+			float len = length();
+			x /= len;
+			y /= len;
+
+			return *this;
+		}
+
+		Vector2 Rotate(Vector2 vector, float degree)
+		{
+			float radian = (degree / 100.0f) * PI;
+			vector.normalize();
+			float x = cosf(radian) * vector.x - sinf(radian) * vector.y;
+			float y = sinf(radian) * vector.x - cosf(radian) * vector.y;
+
+			return Vector2(x, y);
+		}
+
+		float Dot(Vector2& v1, Vector2 v2)
+		{
+			return v1.x * v2.x + v1.y * v2.y;
+		}
+
+		float Cross(Vector2 v1, Vector2 v2)
+		{
+			return v1.x * v2.y - v1.y * v2.x;
 		}
 	};
 }
